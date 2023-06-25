619ea110efa5b529046c75c76e2d9e359cf7ace8
shin <shin@bigpearl.io>
2023-06-25 17:44:30 +0900

    Add simple code for compare sum_to_n code

--- python_rust_module/main.py
+++ python_rust_module/main.py
@@ -1,3 +1,25 @@
 import rust_module
+import time
 
-print(rust_module.sum_as_string(1, 2))
+def time_elapsed(fn):
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        result = fn(*args, **kwargs)
+        elapsed = time.time() - start
+        print(f"elapsed time: {elapsed}")
+        return result
+    return wrapper
+
+@time_elapsed
+def python_sum_to_n(n):
+    return sum(range(1, n+1))
+
+@time_elapsed
+def rust_sum_to_n(n):
+    return rust_module.sum_to_n(n)
+
+
+if __name__ == "__main__":
+    for n in [1, 10, 100, 1000, 10000, 100000, 1000000]:
+        print(python_sum_to_n(n))
+        print(rust_sum_to_n(n))
--- src/lib.rs
+++ src/lib.rs
@@ -6,9 +6,20 @@ fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
     Ok((a + b).to_string())
 }
 
+#[pyfunction]
+fn sum_to_n(n: i64) -> PyResult<i64> {
+    // sum of 1 to n with loop
+    let mut sum = 0;
+    for i in 1..n + 1 {
+        sum += i;
+    }
+    Ok(sum)
+}
+
 /// A Python module implemented in Rust.
 #[pymodule]
 fn rust_module(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    m.add_function(wrap_pyfunction!(sum_to_n, m)?)?;
     Ok(())
 }
